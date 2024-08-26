874c481cc5332a15d25a3378b2d8a189f7374605
Abiezaptr <abieza.eresha@gmail.com>
2024-08-26 09:40:40 +0700

    CSS Fix for Text Wrapping

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -32,6 +32,12 @@ class Home extends CI_Controller
 		// Pass the image URL to the view for dynamic background
 		$data['background_image'] = base_url('uploads/images/' . $data['articleReport']['image']);
 
+		$data['specificReport'] = $this->db->select('*')
+			->from('reports')
+			->like('title', 'Are mobile and fixed broadband')
+			->limit(1)
+			->get()
+			->row_array(); // Fetch one row as an array
 
 		// Load the view with the video data
 		$this->load->view('template/landing/header', $data);
