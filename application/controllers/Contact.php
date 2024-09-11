049163e64d16d8f4c288ec736777ba14049aaa38
Abiezaptr <abieza.eresha@gmail.com>
2024-09-11 14:25:30 +0700

    complete the article in the fixed group

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -6,9 +6,10 @@ class Contact extends CI_Controller
 
     public function index()
     {
+        $data['title'] = 'Contact';
         // Load the views with the data
-        $this->load->view('template/content/header');
-        $this->load->view('contact/index');
+        $this->load->view('template/content/header', $data);
+        $this->load->view('contact/index', $data);
         $this->load->view('template/content/footer');
     }
 }
