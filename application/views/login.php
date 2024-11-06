f951c8bf43674a25f97c28ee5b45a69c8d2c5129
Abiezaptr <abieza.eresha@gmail.com>
2024-11-06 10:25:40 +0700

    update uri api

--- application/views/login.php
+++ application/views/login.php
@@ -308,7 +308,7 @@
                 </label>
                 <input type="password" name="password" placeholder="Masukkan password / Enter your password" required>
                 <br>
-                <a href="https://192.168.0.127:5000/login?app_type=portalnews" class="google-login">
+                <a href="https://10.2.114.197:5000/login?app_type=portalnews" class="google-login">
                     <img src="<?= base_url('assets') ?>/images/microsoft.png" width="20" alt="Google Icon"> Sign in with Microsoft
                 </a>
                 <br>
