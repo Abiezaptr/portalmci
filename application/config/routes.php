f54ca99b6ad18d167af93bc4dd32f436a81fb19a
Abiezaptr <abieza.eresha@gmail.com>
2024-12-22 11:24:30 +0700

    merge libraries to portal

--- application/config/routes.php
+++ application/config/routes.php
@@ -113,3 +113,8 @@ $route['log-history'] = 'login/login_history';
 // Rute untuk ai-assistant
 $route['assistant'] = 'chat/index';
 $route['chatbot'] = 'chat/chatbot';
+
+// Rute untuk users management
+$route['user-management'] = 'users/index';
+$route['add-permission'] = 'users/view_add_permission';
+$route['process-permission'] = 'users/process_add_permission';
